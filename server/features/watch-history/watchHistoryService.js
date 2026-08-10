19fa8bbd4b51e6cffd85452e01e5e216c7934679
raflimuhammadd <raflimuhh09@gmail.com>
2026-08-10 20:29:23 +0700

    refactor: move watch-history feature to features/watch-history/

--- server/features/watch-history/watchHistoryService.js
+++ server/features/watch-history/watchHistoryService.js
@@ -1,6 +1,6 @@
-const db = require('../config/database');
-const { NotFoundError, ValidationError } = require('../utils/error');
-const { MESSAGES } = require('../utils/constant');
+const db = require('../../config/database');
+const { NotFoundError, ValidationError } = require('../../utils/error');
+const { MESSAGES } = require('../../utils/constant');
 
 class WatchHistoryService {
     /**
